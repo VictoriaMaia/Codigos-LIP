31f02c284c675aa535f83eb3d8229797982228a0
victoria_maia <victoria.maia@alu.ufc.br>
2020-10-06 21:56:56 -0300

    Update 3.py

--- Trabalho Funcional/3.py
+++ Trabalho Funcional/3.py
@@ -8,4 +8,4 @@ pertence = lambda x, L: len(L) > 0 and (x == L[0] or pertence(x, L[1:]))
 selecionar = lambda L1, L2: filter(lambda x: not(pertence(x, L2)), L1)
 dif_simetrica = lambda L1, L2: list(selecionar(L1, L2)) + list(selecionar(L2, L1))
 
-print(dif_simetrica(input("Lista1\n").split(" "), input("Lista1\n").split(" ")))
+print(dif_simetrica(input("Lista1\n").split(" "), input("Lista2\n").split(" ")))
